e5c02fe5e8960a0c56d6bdcfd7135191aa4aa4e8
Mélanie Truffy <melanie.tr@hotmail.fr>
2023-06-15 22:41:17 +0200

    fonction suppression work dans modale

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,52 +1,59 @@
 async function loginUser() {
+  //requete HTTP POST API
+  const formConnect = document.querySelector("#login form");
+  const formData = new FormData(formConnect);
+  const response = await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: formData.get("email"),
+      password: formData.get("password"),
+    }),
+  });
 
-    //requete HTTP POST API
-    const formConnect = document.querySelector("#login form");
-    const formData = new FormData(formConnect);
-    const response = await fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: {
-          "Accept": "application/json",
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          "email": formData.get("email"),
-          "password": formData.get("password")
-        })
-      });
+  if (response.status === 200) {
+    // statut 200, connexion réussie
+    // obtenir le corps de réponse (méthode ci-dessus)
+    const data = await response.json();
+    console.log("Connexion réussie !");
+    // Stockage du token
+    const token = data.token;
+    localStorage.setItem("jwt", token);
+    // Renvoi à la page d'accueil si connexion réussie
+    window.location.href = "index.html";
 
-    if (response.status === 200) { // statut 200, connexion réussie
-        // obtenir le corps de réponse (méthode ci-dessus)
-        const data = await response.json();
-        console.log("Connexion réussie !");
-        // Stockage du token
-        const token = data.token;
-        localStorage.setItem("jwt", token);
-        // Renvoi à la page d'accueil si connexion réussie
-        window.location.href = 'index.html';
-
-        console.log("Connexion réussie !");
-    } else if (response.status === 401) { // erreur 401, identifiants invalides
-        alert("Login ou mot de passe incorrect");
-    } else if (response.status === 404) { // erreur 404, utilisateur non trouvé
-        alert("Utilisateur non trouvé");
-    } else { // autres cas
-        alert("Erreur lors de la connexion, veuillez réessayer ultérieurement");
-      }
+    console.log("Connexion réussie !");
+  } else if (response.status === 401) {
+    // erreur 401, identifiants invalides
+    alert("Login ou mot de passe incorrect");
+  } else if (response.status === 404) {
+    // erreur 404, utilisateur non trouvé
+    alert("Utilisateur non trouvé");
+  } else {
+    // autres cas
+    alert("Erreur lors de la connexion, veuillez réessayer ultérieurement");
+  }
 }
-    
-    //Ecoute du bouton submit
-    const form = document.querySelector("#login form");
-    form.addEventListener('submit', async (event) => {
-        event.preventDefault(); // empeche le rechargement de la page par défaut
-        console.log("demande envoyée");
 
-    const userEmail = document.querySelector("#login-email").value;
-    const UserPassword = document.querySelector("#login-password").value;
+//Ecoute du bouton submit
+const form = document.querySelector("#login form");
+form.addEventListener("submit", async (event) => {
+  event.preventDefault(); // empeche le rechargement de la page par défaut
+  console.log("demande envoyée");
 
-    loginUser();
-    });
+  const userEmail = document.querySelector("#login-email").value;
+  const UserPassword = document.querySelector("#login-password").value;
 
-    
+  loginUser();
+});
 
-
+// // déconnexion mode admin
+// function removeToken() {
+//   const token = localStorage.getItem("jwt");
+//   // Supprime le token du localStorage
+//   localStorage.removeItem("token");
+//   sessionStorage.removeItem("deletedImages");
+// }
--- FrontEnd/js/works.js
+++ FrontEnd/js/works.js
@@ -1,47 +1,85 @@
 async function fetchWorks() {
-    try {
-      const response = await fetch('http://localhost:5678/api/works');
-      const works = await response.json();
+  try {
+    const response = await fetch("http://localhost:5678/api/works");
+    const works = await response.json();
 
-      // Création de l'objet catégories
-      const categories = new Set();
-  
+    // Création de l'objet catégories
+    const categories = new Set();
+
+    for (let i = 0; i < works.length; i++) {
+      const work = works[i];
+      const category = work.category.name;
+      console.log(category);
+
+      // Ajout de la catégorie obtenue à l'objet Set pour obtenir les catégories uniques
+      categories.add(category);
+    }
+
+    // Création de la barre de filtres
+    const filterBar = document.querySelector(".filter");
+
+    // Création du bouton Tous
+    const allButton = document.createElement("p");
+    allButton.textContent = "Tous";
+    filterBar.appendChild(allButton);
+
+    //Affichage de tous les works au clic sur bouton tous
+    allButton.addEventListener("click", () => {
+      const galleryWorks = document.querySelector(".gallery");
+      galleryWorks.innerHTML = "";
+
+      //Récupération des works depuis l'api
       for (let i = 0; i < works.length; i++) {
-        const work = works[i];
-        const category = work.category.name;
-        console.log(category);
+        const element = works[i];
+        console.log(element);
 
-        // Ajout de la catégorie obtenue à l'objet Set pour obtenir les catégories uniques
-        categories.add(category); 
+        // Création de la fiche pour un work
+        const workElement = document.createElement("figure");
+        // Création des éléments composant la fiche
+        const imageElement = document.createElement("img");
+        imageElement.src = element.imageUrl;
+        imageElement.setAttribute("alt", `${element.title}`);
+        const titreElement = document.createElement("figcaption");
+        titreElement.innerText = element.title;
+
+        // Rattachement des balises
+        // On rattache la balise article à la section Fiches
+        galleryWorks.appendChild(workElement);
+        workElement.appendChild(imageElement);
+        workElement.appendChild(titreElement);
       }
-      
-      // Création de la barre de filtres
-      const filterBar = document.querySelector(".filter");
+    });
 
-      // Création du bouton Tous
-      const allButton = document.createElement("p");
-      allButton.textContent = "Tous";
-      filterBar.appendChild(allButton);
-      
-      //Affichage de tous les works au clic sur bouton tous
-      allButton.addEventListener('click', () => {
+    allButton.click();
+
+    // Création des boutons de filtre pour chaque catégorie récupérée via l'objet set
+    categories.forEach((category) => {
+      const filterButton = document.createElement("p");
+      filterButton.textContent = category;
+      filterBar.appendChild(filterButton);
+
+      //Filtrer les works selon la catégorie cliquée
+      filterButton.addEventListener("click", () => {
+        const filteredWorks = works.filter(
+          (work) => category === work.category.name
+        );
+
         const galleryWorks = document.querySelector(".gallery");
         galleryWorks.innerHTML = "";
 
-        //Récupération des works depuis l'api
-        for (let i = 0; i < works.length; i++) {
-          const element = works[i];
-          console.log(element);
-    
+        // Affichage des works filtrés
+        for (let i = 0; i < filteredWorks.length; i++) {
+          const element = filteredWorks[i];
+
           // Création de la fiche pour un work
           const workElement = document.createElement("figure");
           // Création des éléments composant la fiche
           const imageElement = document.createElement("img");
           imageElement.src = element.imageUrl;
-          imageElement.setAttribute("alt",`${element.title}`);
+          imageElement.setAttribute("alt", `${element.title}`);
           const titreElement = document.createElement("figcaption");
           titreElement.innerText = element.title;
-            
+
           // Rattachement des balises
           // On rattache la balise article à la section Fiches
           galleryWorks.appendChild(workElement);
@@ -49,119 +87,101 @@ async function fetchWorks() {
           workElement.appendChild(titreElement);
         }
       });
+    });
 
-      allButton.click();
+    //MODALE
+    //création des constantes
+    const modal = document.querySelector("#worksModal");
+    const modalContent = document.querySelector(".modalContent");
+    const openModalButton = document.querySelector(".openModalButton");
+    const modalGallery = document.querySelector(".modalGallery");
+    const closeModalButton = document.querySelector(".closeModalButton");
 
-      // Création des boutons de filtre pour chaque catégorie récupérée via l'objet set
-      categories.forEach((category) => {
-         const filterButton = document.createElement("p");
-         filterButton.textContent = category;
-         filterBar.appendChild(filterButton);
+    //Ecoute du bouton open
+    openModalButton.addEventListener("click", function () {
+      openModal();
+    });
 
-        //Filtrer les works selon la catégorie cliquée
-         filterButton.addEventListener('click', () => {
-          const filteredWorks = works.filter((work) => category === work.category.name);
-        
-          const galleryWorks = document.querySelector(".gallery");
-          galleryWorks.innerHTML = "";
+    //Ecoute du bouton close
+    closeModalButton.addEventListener("click", function () {
+      closeModal();
+    });
 
-  
-          // Affichage des works filtrés
-          for (let i = 0; i < filteredWorks.length; i++) {
-            const element = filteredWorks[i]
+    //Afficher les travaux dans la modale
+    function showModalGallery() {
+      for (let i = 0; i < works.length; i++) {
+        const element = works[i];
+        const newElement = document.createElement("figure");
+        newElement.setAttribute("data-id", element.id);
+        const newElementImg = document.createElement("img");
+        newElementImg.src = element.imageUrl;
+        const newElementTitle = document.createElement("figcaption");
+        newElementTitle.innerText = "éditer";
 
-            // Création de la fiche pour un work
-            const workElement = document.createElement("figure");
-            // Création des éléments composant la fiche
-            const imageElement = document.createElement("img");
-            imageElement.src = element.imageUrl;
-            imageElement.setAttribute("alt",`${element.title}`);
-            const titreElement = document.createElement("figcaption");
-            titreElement.innerText = element.title;
-        
-            // Rattachement des balises
-            // On rattache la balise article à la section Fiches
-            galleryWorks.appendChild(workElement);
-            workElement.appendChild(imageElement);
-            workElement.appendChild(titreElement);
-          }
+        // Conteneur supplémentaire pour l'image et l'icône de la corbeille
+        const imageContainer = document.createElement("div");
+        imageContainer.classList.add("image-container");
+
+        // Création de l'icône de corbeille
+        const trashIcon = document.createElement("i");
+        trashIcon.classList.add("fa-solid", "fa-trash-can");
+
+        // écouteur d'évènement sur les icones poubelles pour supprimer une image
+        trashIcon.addEventListener("click", function () {
+          const workId = newElement.getAttribute("data-id");
+          deleteWork(workId, newElement);
         });
-      });
 
-      //MODALE 
-      //création des constantes
-      const modal = document.querySelector("#worksModal");
-      const modalContent = document.querySelector(".modalContent");
-      const openModalButton = document.querySelector(".openModalButton");
-      const modalGallery = document.querySelector(".modalGallery");
-      const closeModalButton = document.querySelector(".closeModalButton");
-
-      //Ecoute du bouton open
-      openModalButton.addEventListener('click', function() {
-        openModal();
-      })
-
-       //Ecoute du bouton close
-       closeModalButton.addEventListener('click', function() {
-        closeModal();
-      })
-
-      //Afficher les travaux dans la modale
-      function showModalGallery () {
-        for (let i = 0; i < works.length; i++) {
-          const element = works[i];
-          const newElement = document.createElement("figure");
-          newElement.setAttribute("data-id", element.id);
-          const newElementImg = document.createElement("img");
-          newElementImg.src = element.imageUrl;
-          const newElementTitle = document.createElement("figcaption");
-          newElementTitle.innerText = "éditer";
-
-
-          // Conteneur supplémentaire pour l'image et l'icône de la corbeille
-          const imageContainer = document.createElement("div");
-          imageContainer.classList.add("image-container");
-
-          // Création de l'icône de corbeille
-          const trashIcon = document.createElement("i");
-          trashIcon.classList.add("fa-solid", "fa-trash-can");
-
-          //Rattachement des balises
-          modalGallery.appendChild(newElement);
-          newElement.appendChild(imageContainer)
-          imageContainer.appendChild(newElementImg);
-          imageContainer.appendChild(trashIcon);
-          newElement.appendChild(newElementTitle);
-        
-        }
+        //Rattachement des balises
+        modalGallery.appendChild(newElement);
+        newElement.appendChild(imageContainer);
+        imageContainer.appendChild(newElementImg);
+        imageContainer.appendChild(trashIcon);
+        newElement.appendChild(newElementTitle);
       }
-      //fonction ouvrir la modale
-      const openModal = function(){
+    }
+    //fonction ouvrir la modale
+    const openModal = function () {
       modal.style.display = "flex";
 
-      showModalGallery()
-      }
+      showModalGallery();
+    };
 
-      // fonction fermer la modale
-      const closeModal = function(){
-        modal.style.display = "none";
-        modalGallery.innerHTML = "";
-      }
+    // fonction fermer la modale
+    const closeModal = function () {
+      modal.style.display = "none";
+      modalGallery.innerHTML = "";
+    };
 
-      // Ajoutez un écouteur d'événement pour fermer la modale lorsque l'utilisateur clique en dehors de celle-ci
-      window.addEventListener('click', function(event) {
-        if (event.target === modal) {
+    // Ecouteur d'événement pour fermer la modale lorsque l'utilisateur clique en dehors
+    window.addEventListener("click", function (event) {
+      if (event.target === modal) {
         closeModal();
-        }
-      });
-
+      }
+    });
   } catch (error) {
-  console.error("Une erreur est survenue lors de la récupération des travaux :", error);
+    console.error(
+      "Une erreur est survenue lors de la récupération des travaux :",
+      error
+    );
   }
 }
 
 fetchWorks();
 
-
-
-
+// fonction pour supprimer les travaux
+async function deleteWork(workId, works) {
+  //const userToken = localStorage.getItem("jwt"); // Récupération du token
+  const response = await fetch(`http://localhost:5678/api/works/${workId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("jwt"),
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
+  if (response.ok) {
+    works.remove();
+    document.querySelector(`figure[data-id="${workId}"]`).remove();
+  }
+}
